e5c17ffe4c6e3594df5990a2e4dca84f0a3c8a61
kausgang <write.to.kaustav@gmail.com>
2021-12-15 01:42:22 -0500

    import data

--- routes/loaddata.js
+++ routes/loaddata.js
@@ -12,22 +12,40 @@ var router = express.Router();
 const upload = multer({ dest: "public/uploads/" });
 
 router.post("/", upload.single("fenlist"), function (req, res, next) {
+  // console.log(req.file.filename);
+  // if (req.file.filename == "undefined")
+  //   res.send("go back and select file to import");
+
   server_filename = path.join("public", "uploads", req.file.filename);
   data = fs.readFileSync(server_filename, { encoding: "utf8", flag: "r" });
   // res.send(data);
 
   // delete server file
-  // fs.unlink(server_filename, function (err) {
-  //   if (err) return console.log(err);
-  //   console.log("file deleted successfully");
-  // });
+  fs.unlink(server_filename, function (err) {
+    if (err) return console.log(err);
+    console.log("file deleted successfully");
+  });
 
   var rows = data.trim().split(/\r?\n|\r/); // Regex to split/separate the CSV rows
-  row0 = rows[0].split(",");
-  // split(/>(.*)</)
-  console.log(row0[0].split(/>(.*)</)[1]);
 
-  res.render("index", { data: data });
-  // console.log(data);
+  labels = [];
+  fens = [];
+
+  // res.locals.labels = [];
+  // res.locals.fens = [];
+
+  rows.forEach((element) => {
+    row_data = element.split(",");
+    label = row_data[0].split(/>(.*)</)[1];
+    fen = row_data[1];
+    labels.push(label);
+    fens.push(fen);
+    // res.locals.labels.push(label);
+    // res.locals.fens.push(fen);
+  });
+
+  // res.redirect("/");
+  res.render("index", { labels: labels, fens: fens });
+  // next();
 });
 module.exports = router;
